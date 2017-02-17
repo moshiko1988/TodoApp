e5afee6b1f780cbd1826976693c0f7650d7b23ab
moshiko1988 <moshiko1988@gmail.com>
2017-02-17 10:34:26 -0500

    Remove user id column from lists

--- db/migrate/20170215221937_add_user_id_to_lists.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class AddUserIdToLists < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :lists, :user, foreign_key: true, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215221937) do
+ActiveRecord::Schema.define(version: 20170215194626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 20170215221937) do
     t.text     "note",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.index ["user_id"], name: "index_lists_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,5 +40,4 @@ ActiveRecord::Schema.define(version: 20170215221937) do
   end
 
   add_foreign_key "examples", "users"
-  add_foreign_key "lists", "users"
 end
